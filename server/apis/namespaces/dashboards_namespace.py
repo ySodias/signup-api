b5ba2a10cb7e3dd7956894f3bb3959b57757c4fe
ySodias <63486008+ySodias@users.noreply.github.com>
2023-03-13 21:38:49 -0300

    updated branch

--- server/apis/namespaces/dashboards_namespace.py
+++ server/apis/namespaces/dashboards_namespace.py
@@ -1,4 +1,7 @@
 from flask_restx import Namespace, Resource
+from flask import request
+
+from server import environment
 
 dashboards = Namespace('dashboards')
 
@@ -6,5 +9,15 @@ dashboards = Namespace('dashboards')
 class Dashboards(Resource):
 
     @dashboards.doc('get dashboards')
-    def get(self):
-        return 'it works'
+    def post(self):
+        url = environment.microsoft_url
+        headers = {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Cookie': environment.cookie
+        }
+        payload=f'grant_type=password&scope=openid&' \
+                f'resource=https%3A%2F%2Fanalysis.windows.net%2Fpowerbi%' \
+                f'2Fapi&client_id={environment.azure_client_id}&' \
+                f'username={environment.azure_username}r&password={environment.azure_password}'
+        response = request("POST", url, headers=headers, data=payload)
+        return response.json().get('access_token')
