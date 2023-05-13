9dd4c3f42a03a9eafacb5df9bc59c15cedd3e828
ySodias <63486008+ySodias@users.noreply.github.com>
2023-05-13 08:01:43 -0300

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -18,4 +18,4 @@ api = Api(
 db = SQLAlchemy()
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = environment.database_connection
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://signup:6BWdGl0r21q5TQYxGsn33bgWZ46r5ptr@dpg-cgu0l9aut4mcfrg3teig-a/db_signup_i5l3"
